c9c8efdc35683b44f444efed559cf1c886cceb6e
Sol Puente <124692695+solp22@users.noreply.github.com>
2026-01-21 14:27:54 -0300

    Add files via upload

---
+++ formReg.js
@@ -0,0 +1,29 @@
+import { register } from "./auth.js";
+const regForm = document.querySelector("#register-form");
+
+regForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const email = document.querySelector("#email").value;
+  const contraseña = document.querySelector("#password").value;
+
+  const res = await register( email, contraseña);
+
+  if (res.ok) {
+    mostrarPopup();
+  } else {
+    alert(res.msg);
+  }
+});
+
+function mostrarPopup(redireccion = "index.html") {
+  const popup = document.getElementById("popup");
+  popup.classList.remove("hidden");
+
+  const btn = document.getElementById("popupBtn");
+
+  btn.onclick = () => {
+    popup.classList.add("hidden");
+    window.location.href = redireccion;
+  };
+}
